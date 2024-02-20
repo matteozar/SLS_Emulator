fb8e97ae613784560745128bb1a08573d155d04c
matteozar <148753052+matteozar@users.noreply.github.com>
2024-02-20 17:46:59 +0100

    makes the code more readable and efficient, allows you to continue with the "next" button after discovering the device

--- emulator.py
+++ emulator.py
@@ -1,20 +1,42 @@
-#source port aumenta di 1 dopo ogni messaggio da gui
-
-import socket
-import time
-import re
 from scapy.all import *
 import threading
 import message_generator
 from reply import *
 
+GUI_IP = "192.168.0.105"
 payload_bytes = b""
 source_port = 0
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(("192.168.0.105", 4088))
+sock.bind((GUI_IP, 4088))
 sock1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock1.bind(("192.168.0.105", 3000))
+sock1.bind((GUI_IP, 3000))
+
+
+def right_shift_to_the_first_one(decimal_number):
+    binary_number = bin(decimal_number)[2:]
+    count = 0
+
+    for bit in reversed(binary_number):
+        if bit == '0':
+            count += 1
+        else:
+            break
+    shifted_binary_number = '0' * count + binary_number[:-count]
+    try:
+        shifted_decimal_number = int(shifted_binary_number, 2)
+    except ValueError:
+        shifted_decimal_number = 0
+    return shifted_decimal_number
+
+
+def get_info_task():
+    global payload_bytes
+
+    while b"DLA_DISCOVERY;v1.0;FIND_REQ;" not in payload_bytes:
+        source_port_temp, payload_bytes_temp = wait_for_message(b"DLA_DISCOVERY;v1.0;DIAG_REQ;MAC=")
+        sock.sendto(sls_info_reply.encode('utf-8'), (GUI_IP, source_port_temp))
+        print(f"reply: {sls_info_reply}")
 
 
 def send_discovery(device_family, device_model, device_protocol):
@@ -27,9 +49,24 @@ def send_discovery(device_family, device_model, device_protocol):
                         f"SwVersion=03.02.00.66;MibSchemaVersion=8.0.0;Status=Running=on-line;" \
                         f"SubnetMask=255.255.255.0;GatewayAddress=192.168.0.1;Dns1Address=8.8.8.8;" \
                         f"Dns2Address=8.8.4.4;UseDhcp=False;SlavesNumber=0;"
-        sock.sendto(discovery_res.encode('utf-8'), ("192.168.0.105", GUI_PORT))
+        sock.sendto(discovery_res.encode('utf-8'), (GUI_IP, GUI_PORT))
         print(f"> {discovery_res}")
         time.sleep(1 / 2)
+
+
+def wait_for_message(byte_message=b"", previous_reply=b"impossible", previous_answer=b"impossible"):
+    global payload_bytes
+    global source_port
+
+    payload_bytes = b""
+    source_port = 0
+    while byte_message not in payload_bytes or payload_bytes == b"" or payload_bytes == previous_reply or payload_bytes == previous_answer:
+        pass
+    payload_bytes_temp = payload_bytes
+    source_port_temp = source_port
+    print(f"message accepted: {payload_bytes}\n")
+
+    return source_port_temp, payload_bytes_temp
 
 
 def state_machine():
@@ -37,83 +74,56 @@ def state_machine():
     global source_port
 
     while True:
-        while b"DLA_DISCOVERY;v1.0;FIND_REQ;" not in payload_bytes:
-            pass
-        print("Connection request received!")
+        # discovery messages
+        wait_for_message(b"DLA_DISCOVERY;v1.0;FIND_REQ;")
         send_discovery("SLS-M5-0812-E", "958001110", "SLS")
 
-        while b"DLA_DISCOVERY;v1.0;FIND_REQ;AnswerPort=" not in payload_bytes:
-            pass
-        print("Connection request received!")
-        payload_bytes_temp = payload_bytes
-        source_port_tmp = source_port
-        match = re.search(r'AnswerPort=(\d+)', payload_bytes_temp.decode('utf-8'))
-        answer_port = int(match.group(1))
-        sock.sendto(sls_connection_reply.encode('utf-8'), ('192.168.0.105', source_port_tmp))
-        print("Connection response sent!")
-        print("Answer Port acquired:", source_port_tmp)
+        # discovery + answerport messages
+        source_port_temp, payload_bytes_temp = wait_for_message(b"DLA_DISCOVERY;v1.0;FIND_REQ;AnswerPort=")
+        sock.sendto(sls_connection_reply.encode('utf-8'), (GUI_IP, source_port_temp))
 
-        payload_bytes = b""
-
-        while payload_bytes == b"" or payload_bytes == sls_connection_reply.encode('utf-8'):
-            pass
-        print("Password request received!")
-        payload_bytes_temp = payload_bytes
-        source_port_tmp = source_port
-        seqnum = int.from_bytes(payload_bytes_temp[4:8], byteorder='big') >> 24
+        # password request messages
+        print("--- waiting for password request ---")
+        source_port_temp, payload_bytes_temp = wait_for_message(previous_reply=sls_connection_reply.encode('utf-8'))
+        print(" --- password request received ---")
+        seqnum = int.from_bytes(payload_bytes_temp[4:8], byteorder='little')
+        print(f"seqnum: {seqnum}")
         reply = message_generator.getPwAndAcReply(seqnum, 0, "admin\0", 1).pack()
-        sock1.sendto(reply, ('192.168.0.105', source_port_tmp))
-        print("Password response sent!")
-        print("Answer Port password message:", source_port_tmp)
+        sock1.sendto(reply, (GUI_IP, source_port_temp))
+        print(f"reply: {reply}")
 
-        payload_bytes = b""
-
-        while payload_bytes == b"" or payload_bytes == reply:
-            pass
-        print("GetStatus request received!")
-        payload_bytes_temp = payload_bytes
-        source_port_tmp = source_port
-        seqnum = int.from_bytes(payload_bytes_temp[4:8], byteorder='big') >> 24
+        # getstatus request messages
+        print("--- waiting for getstatus request ---")
+        source_port_temp, payload_bytes_temp = wait_for_message("\x03\x00\x00\x00".encode('utf-8'))
+        print(" --- getstatus request received ---")
+        seqnum = int.from_bytes(payload_bytes_temp[4:8], byteorder='little')
+        print(f"seqnum: {seqnum}")
         reply = message_generator.getStatusReply(seqnum, 0, 0, 0).pack()
-        sock1.sendto(reply, ('192.168.0.105', source_port_tmp))
-        print("Getstatus response sent!")
-        print("Answer Port getstatus message:", source_port_tmp)
+        sock1.sendto(reply, (GUI_IP, source_port_temp))
+        print(f"reply: {reply}")
 
-        while b"DLA_DISCOVERY;v1.0;DIAG_REQ;MAC=" not in payload_bytes:
-            pass
-        print("Info request received!")
-        source_port_tmp = source_port
-        sock.sendto(sls_info_reply.encode('utf-8'), ('192.168.0.105', source_port_tmp))
-        print("Info response sent!")
-        print("Answer Port info message:", source_port_tmp)
+        # get device info cyclic messages
+        threading.Thread(target=get_info_task).start()
 
-        payload_bytes = b""
-
-        while "\x2c\x00\x00\x00".encode('utf-8') not in payload_bytes:
-            pass
-        print("HW version request received!")
-        payload_bytes_temp = payload_bytes
-        source_port_tmp = source_port
-
-        seqnum = int.from_bytes(payload_bytes_temp[4:8], byteorder='big') >> 24
+        # get-hw-versions messages
+        print("--- waiting for get-hw-versions request ---")
+        source_port_temp, payload_bytes_temp = wait_for_message("\x2c\x00\x00\x00".encode('utf-8'))
+        print(" --- get-hw-versions request received ---")
+        seqnum = int.from_bytes(payload_bytes_temp[4:8], byteorder='little')
+        print(f"seqnum: {seqnum}")
         reply = message_generator.getHWVerReply(seqnum, 0, sls_get_hw_reply).pack()
-        sock1.sendto(reply, ('192.168.0.105', source_port_tmp))
-        print("HW version response sent!")
-        print("Answer Port HW version message:", source_port_tmp)
+        sock1.sendto(reply, (GUI_IP, source_port_temp))
+        print(f"reply: {reply}")
 
-        payload_bytes = b""
-
-        while True:
-            while b"DLA_DISCOVERY;v1.0;DIAG_REQ;MAC=" not in payload_bytes:
-                pass
-            source_port_tmp = source_port
-            print("Info request received!")
-            sock.sendto(sls_info_reply.encode('utf-8'), ('192.168.0.105', source_port_tmp))
-            print("Info response sent!")
-            print("Answer Port info message:", source_port_tmp)
-
-            answer_port += 1
-            payload_bytes = b""
+        # getstatus request messages
+        print("--- waiting for getstatus request ---")
+        source_port_temp, payload_bytes_temp = wait_for_message("\x03\x00\x00\x00".encode('utf-8'))
+        print(" --- getstatus request received ---")
+        seqnum = int.from_bytes(payload_bytes_temp[4:8], byteorder='little')
+        print(f"seqnum: {seqnum}")
+        reply = message_generator.getStatusReply(seqnum, 0, 0, 0).pack()
+        sock1.sendto(reply, (GUI_IP, source_port_temp))
+        print(f"reply getstatus sent: {reply}")
 
 
 def packet_handler(pkt):
@@ -121,12 +131,11 @@ def packet_handler(pkt):
     global source_port
 
     if IP in pkt and UDP in pkt:
-        if "192.168.0.105" not in pkt[IP].src:
+        if GUI_IP not in pkt[IP].src:
             return
-        print(pkt.summary())
         payload_bytes = bytes(pkt[UDP].payload)
         source_port = pkt[UDP].sport
-        print(f"    {payload_bytes}\n")
+        #print(f"{payload_bytes}\n")
 
 
 def start_sniffer(interface):
@@ -139,7 +148,7 @@ def print_interfaces_list():
 
 
 if __name__ == "__main__":
-    print_interfaces_list()
+    # print_interfaces_list()
     interface = "Software Loopback Interface 1"
     threading.Thread(target=state_machine).start()
     start_sniffer(interface)
